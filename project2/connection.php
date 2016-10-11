0cdd746cbeefcbe479ad4b6c6aa5f4d65b4282c6
Justin Kusz <justin.kusz@gmail.com>
2016-10-11 03:33:19 -0400

    display db errors

--- project2/connection.php
+++ project2/connection.php
@@ -2,7 +2,6 @@
 	$config = parse_ini_file('/var/www/config.ini');
     $con = new mysqli("localhost", $config['db_username'], $config['db_password'], $config['dbname']);
     if($con->connect_error){
-        echo 'error';
-        die('Connect Error');
+        die($con->connect_error);
     }
 ?>
